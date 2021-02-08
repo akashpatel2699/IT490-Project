fb7b20502f38c397b6111cb9ed5e509ecbb2624f
Akash Patel <amp228@njit.edu>
2021-02-08 15:01:38 -0500

    Basic Navbar added along with material ui installed for the styled react componenets

--- front_end/pages/index.js
+++ front_end/pages/index.js
@@ -3,21 +3,17 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Math Placement Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <div>
         <h1 className={styles.title}>
           Hello World
-        </h1>
-      </main>
-
-      <footer className={styles.footer}>
-        Copyright &copy; 2021
-      </footer>
-    </div>
+          </h1>
+        </div>
+    </>
   )
 }
