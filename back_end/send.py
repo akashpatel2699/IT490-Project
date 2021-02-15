aa6f88ad8798dc0ba2155c319868e1e1e83738dd
Akash Patel <amp228@njit.edu>
2021-02-15 00:00:53 -0500

    Removed comments from back_end receive.py and send.py. Removed static MSG_USER and MSG_PASS under back-end service define and edit default messaging username and password environment variable to match back-end username and password for messaging.

--- back_end/send.py
+++ back_end/send.py
@@ -1,33 +1,9 @@
-"""
+import os
+import time
 import pika
-import os
-import time
 
-time.sleep(20)
-HOST=os.environ.get("MESSAGING_HOST")
-parameters = pika.URLParameters('amqp://guest:guest@{}:5672/%2f'.format(HOST))
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-#calling the channel and passing the queue name
-channel.queue_declare(queue='test-queue')
-
-channel.basic_publish('',
-                        'test-queue',
-                        'message body',
-                        pika.BasicProperties(content_type='text/plain',
-                                          delivery_mode=1))
-
-#channel.basic_publish(exchange="", routing_key="test-queue", body="Hello World")
-connection.close()
-"""
-import pika 
-import os
-import time
-
-time.sleep(45)
 MSG_USER=os.environ.get("MSG_USER")
 MSG_PASS=os.environ.get("MSG_PASS")
-#NAME=os.environ.get("TEST")
 HOST=os.environ.get("MESSAGING_HOST")
 
 credentials = pika.PlainCredentials(MSG_USER,MSG_PASS)
@@ -40,6 +16,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue="hello")
 
-channel.basic_publish(exchange="",routing_key="hello",body="Hello World")
+channel.basic_publish(exchange="",routing_key="hello",body="Good morning, milestone 2 testing")
 
 connection.close()
